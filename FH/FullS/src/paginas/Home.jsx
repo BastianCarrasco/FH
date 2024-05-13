9539117a40abe20a86958e22c3406eea722ab4aa
camitwrs <milatorresv23@gmail.com>
2024-05-12 23:26:30 -0400

    actualizacion de heros y cierre de version

--- FH/FullS/src/paginas/Home.jsx
+++ FH/FullS/src/paginas/Home.jsx
@@ -9,8 +9,7 @@ function Home() {
         <div className="w-1/2 flex flex-col justify-center items-start p-12 text-white">
           <h1 className="text-6xl mb-6 font-montserrat">Tu negocio en buenas manos</h1>
           <p className="text-2xl font-poppins mb-4">Soluciones tecnológicas para pequeñas empresas</p>
-          {/* DEBE LLEVAR AL MISMO CARACTERISTICAS DEL NAVBAR */}
-          <a href="/caracteristicas" className="text-orange-400 font-montserrat text-xl hover:text-orange-600">Más información →</a>
+  
         </div> 
         <div className="w-1/2 flex items-center justify-center">
         <img src={ipad_img} alt="Imagen de ipad" />
--- FH/FullS/src/paginas/compañia.jsx
+++ FH/FullS/src/paginas/compañia.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import pymeimg from '../assets/pyme.png'
-import flechita from '../assets/icons8-down-96.png'
 
 function Compania() {
   return (
     <div>
-      <section className="bg-fondo h-screen">
-      <div className="flex h-screen w-full items-center justify-center flex-col mt-10">
-        <h1 className="text-6xl mb-6 font-montserrat text-white mt-10">Tu negocio en buenas manos</h1>
-        <p className="text-2xl text-white font-poppins mb-4">Soluciones tecnológicas para pequeñas empresas</p>
-        <img className="w-12 h-12" src={flechita}/> 
-      </div>
-      
-      </section>
-
       <section>
       <div className="flex h-screen w-full bg-fondo-light">
         <div className="w-1/2 flex items-center justify-center">
